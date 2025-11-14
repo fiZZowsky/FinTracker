171a8daa55125a824d5d28b9c218ee12329ab669
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-11-14 16:01:18 +0100

    Implemented receipt data searching

--- FinTracker.Services/ReceiptService.cs
+++ FinTracker.Services/ReceiptService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using FinTracker.Models;
 using FinTracker.Repositories;
+using System.Globalization;
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace FinTracker.Services
 {
@@ -50,34 +52,138 @@ namespace FinTracker.Services
 
         private Receipt _ParseTextToReceipt(string ocrText)
         {
-            // TODO: Zaimplementuj logikę parsowania (np. używając Regex)
-
-            string storeName = "Nieznany Sklep";
             decimal totalAmount = 0.0m;
+            DateTime dateShopping = DateTime.UtcNow;
 
-            var match = Regex.Match(ocrText,
-                @"(SUMA:|RAZEM:|KWOTA:|DO ZAPŁATY:)\s*(PLN)?\s*(\d+[\s,.]+(\d{2}|S{2}|O{2}))",
-                RegexOptions.IgnoreCase);
-            if (match.Success)
+            string storeName = _ParseStoreName(ocrText);
+
+            var totalMatch = Regex.Match(ocrText,
+                @"(SUMA:|RAZEM:|SUMA PLN|KWOTA:|DO ZAPŁATY:)\s*(PLN)?\s*(\d+[\s,.]+(\d{2}|S{2}|O{2}))",
+                RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            if (totalMatch.Success)
             {
-                string amountStr = match.Groups[3].Value
-                    .Replace(',', '.') 
+                string amountStr = totalMatch.Groups[3].Value
+                    .Replace(',', '.')
                     .Replace('S', '5')
                     .Replace('O', '0')
                     .Replace(" ", "");
-                
+
                 decimal.TryParse(amountStr,
-                    System.Globalization.NumberStyles.Any,
-                    System.Globalization.CultureInfo.InvariantCulture,
+                    NumberStyles.Any,
+                    CultureInfo.InvariantCulture,
                     out totalAmount);
+            }
+            
+            var dateMatch = Regex.Match(ocrText, @"(\d{4}-\d{2}-\d{2})|(\d{2}[.-]\d{2}[.-]\d{4})");
+
+            if (dateMatch.Success)
+            {
+                string dateStr = dateMatch.Value;
+                
+                if (DateTime.TryParse(dateStr, CultureInfo.GetCultureInfo("pl-PL"), DateTimeStyles.None, out DateTime parsedDate))
+                {
+                    dateShopping = parsedDate;
+                }
+                else if (DateTime.TryParse(dateStr, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
+                {
+                    dateShopping = parsedDate;
+                }
             }
 
             return new Receipt
             {
                 StoreName = storeName,
                 TotalAmount = totalAmount,
-                DateShopping = DateTime.UtcNow
+                DateShopping = dateShopping
             };
+        }
+
+        private string _ParseStoreName(string ocrText)
+        {
+            var knownStores = new List<string>
+            {
+                "SPOŁEM", "BIEDRONKA", "LIDL", "ŻABKA", "AUCHAN",
+                "CARREFOUR", "KAUFLAND", "ROSSMANN", "HEBE", "NETTO",
+                "ALDI", "STOKROTKA", "INTERMARCHE", "LEWIATAN", "ORLEN"
+            };
+
+            string normalizedOcrText = _RemoveDiacritics(ocrText.ToUpper()).Replace(" ", "");
+
+            const int maxDistanceThreshold = 1;
+
+            foreach (var store in knownStores)
+            {
+                string normalizedStore = _RemoveDiacritics(store.ToUpper());
+                int storeLen = normalizedStore.Length;
+                
+                if (storeLen < 4) continue;
+                
+                for (int i = 0; i <= normalizedOcrText.Length - storeLen; i++)
+                {
+                    string ocrSubstring = normalizedOcrText.Substring(i, storeLen);
+                    int distance = _LevenshteinDistance(ocrSubstring, normalizedStore);
+                    if (distance <= maxDistanceThreshold)
+                    {
+                        return store;
+                    }
+                }
+            }
+            
+            return "Nieznany Sklep";
+        }
+
+        private int _LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0) return m;
+            if (m == 0) return n;
+
+            for (int i = 0; i <= n; d[i, 0] = i++) { }
+            for (int j = 0; j <= m; d[0, j] = j++) { }
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[n, m];
+        }
+
+        private bool _IsAllUpper(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input) || !input.Any(char.IsLetter))
+                return false;
+            
+            return !input.Any(char.IsLower);
+        }
+
+        private string _RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+            
+            return stringBuilder.ToString()
+                .Normalize(NormalizationForm.FormC)
+                .Replace('Ł', 'L')
+                .Replace('ł', 'l');
         }
     }
 }
