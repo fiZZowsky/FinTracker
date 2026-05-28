aa85049a22a2d08d0f8118b9abd61669ee85f8a6
fiZZowsky <piotrekr53@interia.pl>
2026-05-28 19:06:08 +0200

    fix ReceiptParsesService

--- FinTracker.Services/ReceiptParserService.cs
+++ FinTracker.Services/ReceiptParserService.cs
@@ -27,7 +27,8 @@ namespace FinTracker.Services
             DateTime dateShopping = DateTime.UtcNow;
             string currencyCode = "PLN";
 
-            string amountPattern = @"(SUMA|S[U0O]M[A4]|RAZEM|KWOTA|DO\s*ZAP[LŁ1I]ATY|WARTO[SŚ][CĆ]|BRUTTO|TOTAL|T[O0]TAL|AMOUNT|AM[O0]UNT|AMOUNT\s*DUE|BALANCE|DUE)\s*[\s:.;]*\s*(PLN|Z[LŁ1I]|P1N|EUR|USD|€|\$)?\s*([0-9OoSsDdBQ]+(?:[\s][0-9OoSsDdBQ]+)*[.,][0-9OoSs]{2})\s*(PLN|Z[LŁ1I]|P1N|EUR|USD|€|\$)?\b";
+            string amountPattern = @"(SUMA|S[U0O]M[A4]|RAZEM|KWOTA|DO\s*ZAP[LŁ1I]ATY|WARTO[SŚ][CĆ]|BRUTTO|TOTAL|T[O0]TAL|AMOUNT|AM[O0]UNT|AMOUNT\s*DUE|BALANCE|DUE)"
+                + @"\s*[\s:.;]*\s*(PLN|Z[LŁ1I]|P1N|EUR|USD|€|\$)?\s*([0-9OoSsDdBQ]+(?:[\s][0-9OoSsDdBQ]+)*[.,][0-9OoSs]{2})\s*(PLN|Z[LŁ1I]|P1N|EUR|USD|€|\$)?\b";
             var matches = Regex.Matches(ocrText, amountPattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
 
             if (matches.Count > 0)
@@ -44,8 +45,6 @@ namespace FinTracker.Services
                 currencyCode = MapCurrencyToIsoCode(currencyMatch);
             }
 
-            var exchangeRate = await _exchangeRateService.GetRateAsync(currencyCode, dateShopping);
-
             var dateMatch = Regex.Match(ocrText, @"(\d{4}-\d{2}-\d{2})|(\d{2}[.-]\d{2}[.-]\d{4})");
             if (dateMatch.Success)
             {
@@ -59,6 +58,8 @@ namespace FinTracker.Services
                     dateShopping = parsedDate;
                 }
             }
+
+            var exchangeRate = await _exchangeRateService.GetRateAsync(currencyCode, dateShopping);
 
             string storeName = await _ParseStoreName(ocrText);
 
